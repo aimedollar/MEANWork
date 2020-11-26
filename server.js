47aece1e6ad68d12b90de355ce2589adfc91b65a
Aime <aimeserge@gmail.com>
2020-11-25 20:43:09 -0600

    Creating a server with node.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+http.createServer( (req, res)=> {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write("what up fox, server is running? \n")
+    res.end();
+}).listen(3000);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:3000/');
