acb06f92286f9f217e1d783e14dfd3f29353ae2d
aime <aimeserge@gmail.com>
2020-11-26 02:25:33 -0600

    update create server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer( (req, res)=> {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write("what up fox, server is running? \n")
     res.end();
-}).listen(3000);
+}).listen(8000);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://127.0.0.1:8000/');
